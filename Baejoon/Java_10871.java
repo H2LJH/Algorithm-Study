44f45ac1d42e1c8be47e9db72d835271974f38f7
ljh93 <ljh93@naver.com>
2020-05-31 23:19:47 +0900

    Test

---
+++ Baejoon/Java_10871.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Java_10871 
+{
+    public static void main(String[] args) 
+    {
+            Scanner sc = new Scanner(System.in);
+            int num = sc.nextInt();
+            sc.close();
+    }    
+}
---
+++ Baejoon/Java_2439.java
@@ -0,0 +1,48 @@
+<<<<<<< HEAD
+import java.util.Scanner;
+
+public class Java_2439 
+{
+        // 백준 2439번 별찍기
+        public static void main(String[] args) 
+    {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            sc.close();
+             
+            for (int i = 1; i <= n; ++i) 
+            {
+                for (int j = 1; j <= n - i; ++j)
+                    System.out.print(" ");
+                 
+                for (int j = 1; j <= i; ++j) 
+                    System.out.print("*");
+
+                System.out.println(); 
+            }
+    }    
+=======
+import java.util.Scanner;
+
+public class Java_2439 
+{
+        // 백준 2439번 별찍기
+        public static void main(String[] args) 
+    {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            sc.close();
+             
+            for (int i = 1; i <= n; ++i) 
+            {
+                for (int j = 1; j <= n - i; ++j)
+                    System.out.print(" ");
+                 
+                for (int j = 1; j <= i; ++j) 
+                    System.out.print("*");
+
+                System.out.println(); 
+            }
+    }    
+>>>>>>> a098cc04dfa9b089f109fa072da6ffd36580fea0
+}
