e92a9cba55a8491f8e52c9417ee3225562e5608c
ljh93 <ljh93@naver.com>
2020-06-15 00:35:04 +0900

    2020-06-14

---
+++ Baekjoon/Java/Java_4344.java
@@ -0,0 +1,23 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Java_4344 
+{
+    public static void main(String[] args) 
+    {
+            Scanner sc = new Scanner(System.in);
+            int c = sc.nextInt(); // 테스트 케이스 
+            while(c != 0)
+            {
+                c--;
+
+                for(int i=0; i<0; ++i)
+                {
+                    
+                }
+
+
+            }
+    }    
+}
